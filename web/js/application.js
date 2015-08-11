930f3eac27b0b2e4fa0530c1677233246c810274
Sourav Mondal <souravm@capitalnumbers.com>
2015-08-11 14:19:02 +0530

    formated html of result

--- web/js/application.js
+++ web/js/application.js
@@ -23,7 +23,7 @@ $('.getWeather').click(function (event) {
                 return;
             } else {
                 var weatherResult = '<div class="alert alert-info spacer" role="alert">';
-                weatherResult += '<br/><strong>Humidity</strong>: ' + data.humidity + ' %';
+                weatherResult += '<strong>Humidity</strong>: ' + data.humidity + ' %';
                 weatherResult += '<br/><strong>Current Temprature</strong>: ' + data.temp + ' °C';
                 weatherResult += '<br/><strong>Max temp</strong>: ' + data.temp_max + ' °C';
                 weatherResult += '<br/><strong>Min temp</strong>: ' + data.temp_min + ' °C';
